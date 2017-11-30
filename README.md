563fa7cd3bdaa784553f539f057535b45ab950b6
VidyaSankaran76 <31882504+VidyaSankaran76@users.noreply.github.com>
2017-11-30 22:01:40 +0000

    Code for cleaning an dataset

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# DataScienceJHU
-JHU Assignments
+#Getting and Cleaning Data - Project
+
+## Getting the data from text files
+* Downloaded the data set and unzipped the same in working directory
+* Visually inspected the files to understand the dataset 
+* Found that X had the signals information from samsung smart phone and Y had the
+information of what activities were performed
+* Provided the right path, the data files were read for train and test sets as numeric form for X variables and factors for Y variables.
+* saved the read X tables and Y tables in different data frames
+
+## Combining train and Test sets
+* Using rbind function in R the train and test sets of X  and Y were merged
+
+
+## Naming the columns and cleaning columns
+
+* Y set had activity information and hence the name was choosen as activity and assinged using colnames()
+
+* X set had 561 variables, and it mached the list availble in the features.txt file.
+* names were read from that file and the names are modified using find and replace functions like gsub(), paste0 functions.
+
+* Unique variable names were coined by adding the column numbers to the string of variables. 
+
+## Renaming factors for new attributes
+
+*  For y sets, the levels are 6. They represent activities like walking, walking_upstairs, walking_downstairs, sitting, standing and laying.
+
+* Attribute function of levels are set with above mentioned activities and now the
+dataset contains meaningful activity names instead of 1 to 6 numeric numbers.
+
+
+## Merging data sets to get clean data
+
+* Above cleaned x and y sets were merged using cbind() function and retained column names
+
+## Obtaining a dataset from subset of the merged data
+
+* From above dataset, read the common columns of smart phone interial signals, 
+and combined them using the means and average. rowMeans() function was used for
+every group available in the subset and average mean for the variables formed 
+the columns
+
+## Finding average alias mean on data
+
+* For every activity, the columns means were computed and stored in the text files.
+
+
+## storing results
---
+++ run_analysis.R
@@ -0,0 +1,128 @@
+library(dplyr)
+library(readr)
+library(reshape2)
+library(stringr)
+
+
+
+
+
+#Reading X train and X test sets
+read_tx_as_DF_X <- read.table("UCIHARDataset/train/X_train.txt", colClasses = "numeric")
+read_tes_as_DF_X <- read.table("UCIHARDataset/test/X_test.txt", colClasses = "numeric")
+
+# Merging x - train and test sets
+x_row <- rbind(read_tx_as_DF_X, read_tes_as_DF_X)
+
+
+#Reading Y train and Y test sets
+read_tx_as_DF_Y <- read.table("UCIHARDataset/train/Y_train.txt", colClasses = "factor")
+read_tes_as_DF_Y <- read.table("UCIHARDataset/test/Y_test.txt", colClasses = "factor")
+
+# Replacing Y levels to activities
+summary(read_tx_as_DF_Y)
+summary(read_tes_as_DF_Y)
+activities <- c("walking", "walking_downstairs","walking_upstairs","sitting","standing","laying")
+colnames(read_tx_as_DF_Y) <- c("activity")
+attr(read_tx_as_DF_Y$activity, "levels") <- activities
+
+colnames(read_tes_as_DF_Y) <- c("activity")
+attr(read_tes_as_DF_Y$activity, "levels") <- activities
+
+# Merging Y - train and test sets
+y_row <- rbind(read_tx_as_DF_Y, read_tes_as_DF_Y)
+
+
+# Names read from features text doc and name x_row data set
+nam_es <- read.table("UCIHARDataset/features.txt")
+nam_es
+
+names(nam_es)
+
+# changing column names for proper meaning
+x <- as.character(gsub("\\()", "", nam_es$V2))
+x <- as.character(gsub(",", "_", x))
+x <- as.character(gsub("-", "_", x))
+x <- as.character(gsub("fBodyBody", "fBody", x))
+x <- as.character(gsub("Mean", "mean", x))
+x<- paste0(x,"_",c(1:length(x)))
+
+
+length(x)
+colnames(x_row) <- x
+
+#Filtering the dataset for mean and std from x_row
+f_o_std <- filter(x_row[(grepl("std", x))] )
+
+names(f_o_std)
+
+f_o_mean <- filter(x_row[(grepl("mean", x))] )
+
+names(f_o_mean)
+
+
+#creating a dat set 
+myDF <- cbind(y_row, f_o_mean, f_o_std)
+
+test_names <- names(myDF)
+
+test_names
+
+#writing the data set in csv file
+#write.csv(myDF, "./final_out.csv")
+
+
+#Reading the features from the features info
+lines_txt <- readLines("UCIHARDataset/features_info.txt", 30)
+tmp <- lines_txt[13:29]
+x_col_names = c()
+
+#creating final Data Frame for the average of all subjects per
+# activity
+finalDF <-data.frame(row.names=1:length(y_row))
+finalDF <- cbind(finalDF, y_row)
+head(finalDF, n=10)
+j=1
+x_col_names[j] <- colnames(y_row) 
+#Make a list without XYZ by combining their average for mean
+for (i in 1:length(tmp)){
+  val <- tmp[i]
+  val <- strsplit(val, "-XYZ")[[1]]
+  val <-  paste0(val,"_mean")
+  #use val to grep the column names and 
+  tmp1 <- myDF %>% select(contains(val)) 
+  x_col_names[j+1] <- val
+  j=j+1
+  y <- rowMeans(tmp1)
+  finalDF <- cbind(finalDF, y)
+  
+}
+
+#Make a list without XYZ by combining their average for std
+for (i in 1:length(tmp)){
+  val <- tmp[i]
+  val <- strsplit(val, "-XYZ")[[1]]
+  val <-  paste0(val,"_std")
+  #use val to grep the column names and 
+  tmp1 <- myDF %>% select(contains(val)) 
+  x_col_names[j+1] <- val
+  j=j+1
+  y <- rowMeans(tmp1)
+  finalDF <- cbind(finalDF, y)
+}
+
+colnames(finalDF) <- x_col_names
+                    
+head(finalDF, n=3)  
+dim(finalDF)
+
+
+summary(finalDF)
+
+myf_d <- finalDF %>% group_by(activity) %>% summarise_if(is.numeric, mean, na.rm = TRUE)
+
+head(myf_d, n=6)
+
+write.table(finalDF, "./final_output.txt", col.names=FALSE, row.names = FALSE)
+
+write.table(myf_d, "./final_output2.txt",  row.names = FALSE)
